d45cc5d49dc1187f25ee30b8f035b35e2b09aa49
Marco P <macg.patino@gmail.com>
2024-10-02 02:30:23 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import PokedexContainer from "./components/PokedexContainer";
+
+function App() {
+  return (
+    <div className="main">
+      <PokedexContainer></PokedexContainer>
+    </div>
+  );
+}
+
+export default App;
